4c951af752a6635a445d987e96e43d3b01dbc178
Keith Duncan <keith.duncan@github.com>
2016-08-21 16:18:33 +0100

    Require the buffer class

--- lib/json-projection/parser.rb
+++ lib/json-projection/parser.rb
@@ -23,6 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+require_relative 'buffer'
 require_relative 'events'
 require_relative 'errors'
 require_relative 'fifo'
