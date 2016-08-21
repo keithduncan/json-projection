57e8d4f4fc45a9495d0e5d5ea1c7c37514f0508f
Keith Duncan <keith.duncan@github.com>
2016-08-21 19:38:01 +0100

    Add tests for how I want the API to work

--- test/projector/test_null_projection.rb
+++ test/projector/test_null_projection.rb
@@ -2,9 +2,19 @@ require_relative 'test_helper'
 
 class NullProjection < JsonProjectorTest
 
+  def test_null_object_projection
+    projector = JsonProjection::Projector.new(stream("{\"foo\": \"bar\"}"))
+    assert_equal({"foo" => "bar"}, projector.project(nil))
+  end
+
   def test_empty_object_projection
-    projector = JsonProjection::Projector.new(stream("{}"))
-    assert_equal({}, projector.project(nil))
+    projector = JsonProjection::Projector.new(stream("{\"foo\": \"bar\"}"))
+    assert_equal({}, projector.project({}))
+  end
+
+  def test_filtering_object_projection
+    projector = JsonProjection::Projector.new(stream("{\"foo\": \"bar\", \"qux\": \"quux\"}"))
+    assert_equal({"foo" => "bar"}, projector.project({"foo" => nil}))
   end
 
 end
