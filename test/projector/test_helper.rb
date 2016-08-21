0e51024facc72e587ed9fc225fbdc930be088831
Keith Duncan <keith.duncan@github.com>
2016-08-21 20:28:34 +0100

    Refactor the null projection tests

--- test/projector/test_helper.rb
+++ test/projector/test_helper.rb
@@ -1,9 +1,18 @@
 require_relative '../test_helper'
 
+require 'json'
+
 class JsonProjectorTest < MiniTest::Unit::TestCase
+  include JsonProjection
+
+  parallelize_me!
 
   def stream(str)
     StringIO.new(str)
+  end
+
+  def project(schema, over: "")
+    Projector.new(stream(over.to_json)).project(schema)
   end
 
 end
--- test/projector/test_null_projection.rb
+++ test/projector/test_null_projection.rb
@@ -1,20 +1,35 @@
 require_relative 'test_helper'
 
 class NullProjection < JsonProjectorTest
-
   def test_null_object_projection
-    projector = JsonProjection::Projector.new(stream("{\"foo\": \"bar\"}"))
-    assert_equal({"foo" => "bar"}, projector.project(nil))
+    json = {
+      "foo" => "bar",
+    }
+
+    assert_equal({
+      "foo" => "bar"
+    }, project(nil, over: json))
   end
 
   def test_empty_object_projection
-    projector = JsonProjection::Projector.new(stream("{\"foo\": \"bar\"}"))
-    assert_equal({}, projector.project({}))
+    json = {
+      "foo" => "bar",
+    }
+    assert_equal({}, project({}, over: json))
   end
 
   def test_filtering_object_projection
-    projector = JsonProjection::Projector.new(stream("{\"foo\": \"bar\", \"qux\": \"quux\"}"))
-    assert_equal({"foo" => "bar"}, projector.project({"foo" => nil}))
-  end
+    json = {
+      "foo" => "bar",
+      "qux" => "quux",
+    }
 
+    schema = {
+      "foo" => nil,
+    }
+
+    assert_equal({
+      "foo" => "bar"
+    }, project(schema, over: json))
+  end
 end
