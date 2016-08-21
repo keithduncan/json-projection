3e2cedeb32cc96a364eb756905b2cd8688b97b2d
Keith Duncan <keith.duncan@github.com>
2016-08-21 16:12:02 +0100

    Add initial tests

---
+++ test/test_top_level_structures.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'json-projection'
+
+class JsonProjectionTopLevelTest < Minitest::Test
+  def test_top_level_object
+    events = read_event_stream(StringIO.new("{}"))
+    assert_equal [JsonProjection::StartDocument, JsonProjection::StartObject, JsonProjection::EndObject, JsonProjection::EndDocument], events
+  end
+
+  def test_top_level_array
+    events = read_event_stream(StringIO.new("[]"))
+    assert_equal [JsonProjection::StartDocument, JsonProjection::StartArray, JsonProjection::EndArray, JsonProjection::EndDocument], events
+  end
+
+  private
+
+  def read_event_stream(io)
+    parser = JsonProjection::Parser.new(io)
+
+    events = []
+
+    while events.last != JsonProjection::EndDocument
+      events << parser.next_event
+    end
+
+    events
+  end
+end
