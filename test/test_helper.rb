285a133aaf22ddf9b795ca74d44669bc5522f1ad
Keith Duncan <keith.duncan@github.com>
2016-08-21 17:30:10 +0100

    Move more common structure

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,27 @@ require 'json-projection'
 
 require 'stringio'
 require 'byebug'
+
+class JsonProjectionTest < MiniTest::Unit::TestCase
+
+  def stream(string)
+    StringIO.new(string)
+  end
+
+  def events(string)
+    read_event_stream(stream("{}"))
+  end
+
+  def read_event_stream(io)
+    parser = Parser.new(io)
+
+    events = []
+
+    while events.last != EndDocument.empty
+      events << parser.next_event
+    end
+
+    events
+  end
+
+end
--- test/test_top_level_structures.rb
+++ test/test_top_level_structures.rb
@@ -1,29 +1,14 @@
 require_relative 'test_helper'
 
-class JsonProjectionTopLevelTest < MiniTest::Unit::TestCase
+class TopLevelTest < JsonProjectionTest
   include JsonProjection
 
   def test_top_level_object
-    events = read_event_stream(StringIO.new("{}"))
-    assert_equal [StartDocument.empty, StartObject.empty, EndObject.empty, EndDocument.empty], events
+    assert_equal [StartDocument.empty, StartObject.empty, EndObject.empty, EndDocument.empty], events("{}")
   end
 
   def test_top_level_array
-    events = read_event_stream(StringIO.new("[]"))
-    assert_equal [StartDocument.empty, StartArray.empty, EndArray.empty, EndDocument.empty], events
+    assert_equal [StartDocument.empty, StartArray.empty, EndArray.empty, EndDocument.empty], events("[]")
   end
 
-  private
-
-  def read_event_stream(io)
-    parser = Parser.new(io)
-
-    events = []
-
-    while events.last != EndDocument.empty
-      events << parser.next_event
-    end
-
-    events
-  end
 end
