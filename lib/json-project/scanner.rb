9409927ae9b5f0608bfed313cb45d91ae2833b8c
Keith Duncan <keith.duncan@github.com>
2016-08-21 13:34:01 +0100

    Add more docs on parse methods

--- lib/json-project/scanner.rb
+++ lib/json-project/scanner.rb
@@ -1,4 +1,5 @@
 require_relative 'events'
+require_relative 'errors'
 
 module JsonProject
   # Pull based event scanner.
@@ -12,11 +13,29 @@ module JsonProject
     #
     # Returns nothing.
     def initialize(steam)
+      @stream = stream
+      @buffer = nil
+    end
+
+    # Draw bytes from the stream until an event can be constructed. May raise
+    # IO errors.
+    #
+    # Returns a JsonProject::StreamEvent subclass or raises StandardError.
+    def next_event()
 
     end
 
+    # Advance the stream cursor until after the given event class. This method
+    # considers Object and Array nesting, use it to skip a subset of the input
+    # document stream.
     #
-    def next_event()
+    # If the document is malformed and EOF is reached before the terminating
+    # event, JsonProject::ParseError is raised.
+    #
+    # Returns nothing, may raise StandardError.
+    def read_until(klass)
+
+    end
 
   end
 end
