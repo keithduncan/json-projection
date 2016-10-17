c2f146ff0c4ae90658c4d0fb54ec81cc39af6b18
Keith Duncan <keith.duncan@github.com>
2016-10-17 12:29:22 +1100

    Remove the profiling

--- test/parser/test_file_parse.rb
+++ test/parser/test_file_parse.rb
@@ -15,25 +15,8 @@ class ParseFileTest < Minitest::Test
     file = File.open(file_path, 'r')
 
     begin
-      result = RubyProf.profile do
-        parser = Parser.new(file)
-
-        counter = 1_000
-        loop do
-          counter -= 1
-          break if counter == 0
-          puts counter
-
-          event = parser.next_event
-          puts event.class
-          break if event.is_a?(EndDocument)
-        end
-      end
-
-      # print a graph profile to text
-      printer = RubyProf::CallStackPrinter.new(result)
-      File.open('tmp/profile.html', 'w') do |f|
-        printer.print(f, {})
+      Parser.new(file).each_event do |event|
+        
       end
     ensure
       file.close
